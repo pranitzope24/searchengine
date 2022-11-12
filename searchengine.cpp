79022b77e88784296361cfe9ed61b53aafc7a7f5
Pranit <pranit.t.zope@gmail.com>
2022-11-13 02:55:54 +0530

    complete main program for part 1

--- searchengine.cpp
+++ searchengine.cpp
@@ -7,8 +7,16 @@ int main(int argc, char ** argv){
     if(argc != 5){
         cout << "Wrong Inputs" << endl;
     }
-    else {
-        cout << "Correct Args" << endl;
+    
+    cout << "Pls Wait....." << endl;
+
+    int lineCounter = 0, maxLength = -1, k = atoi(argv[4]);
+    FILE *file = fopen(argv[2],"r");
+
+    if(!file) {
+        cout << "Error Opening File" << endl;
     }
+    fclose(file);
+
     return 0;
 }
